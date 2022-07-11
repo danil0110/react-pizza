bf994ccee78f4048cd17597eb45cc2f181810ab8
danil0110 <danilmail0110@gmail.com>
2022-07-11 11:22:19 +0300

    Add interactivity to "sort by" menu

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,20 @@
+import { useState } from 'react';
+
 const Sort = () => {
+  const [open, setOpen] = useState(false);
+  const [sortByActive, setSortByActive] = useState('rating');
+
+  const sortByLabels = {
+    rating: 'популярности',
+    price: 'цене',
+    title: 'алфавиту'
+  };
+
+  const onChangeSortBy = (value) => {
+    setSortByActive(value);
+    setOpen(false);
+  };
+
   return (
     <div className='sort'>
       <div className='sort__label'>
@@ -15,15 +31,23 @@ const Sort = () => {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>{sortByLabels[sortByActive]}</span>
       </div>
-      <div className='sort__popup'>
-        <ul>
-          <li className='active'>популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className='sort__popup'>
+          <ul>
+            {Object.entries(sortByLabels).map((item, idx) => (
+              <li
+                key={idx}
+                className={item[0] === sortByActive ? 'active' : null}
+                onClick={() => onChangeSortBy(item[0])}
+              >
+                {item[1]}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
