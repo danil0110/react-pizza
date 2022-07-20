8b41f5f62fe7f906dbf3d6b213773b22efb66ca5
danil0110 <danilmail0110@gmail.com>
2022-07-20 17:17:43 +0300

    Add go back button on pizza details page

--- src/pages/PizzaDetails.tsx
+++ src/pages/PizzaDetails.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 import { IPizzaCart, IPizzaResponse } from '../interfaces/pizza.interface';
 import { useAppDispatch } from '../store';
@@ -75,71 +75,76 @@ const PizzaDetails: React.FC = () => {
 
   // TODO: write pleasant styles for PizzaDetails component
   return (
-    <div className='container'>
-      <div className='pizza-details'>
-        <img className='pizza-details__image' src={pizza.imageUrl} alt={pizza.title} />
-        <div className='pizza-details__info'>
-          <h2 className='pizza-details__title'>{pizza.title}</h2>
-          <div className='pizza-details__selector'>
-            <ul>
-              {pizza.types.map((typeId) => {
-                const active = activeType === typeId;
-                return (
-                  <li
-                    key={typeId}
-                    className={active ? 'active' : ''}
-                    onClick={() => onChangeType(typeId)}
-                  >
-                    {typeLabels[typeId]}
-                    {active && typeId !== pizza.types[0] && (
-                      <div className='price-increase'>
-                        +{pizza.prices[typeId][pizza.sizes[0]] - pizza.startPrice} ₴
-                      </div>
-                    )}
-                  </li>
-                );
-              })}
-            </ul>
-            <ul>
-              {pizza.sizes.map((size, idx) => {
-                const active = activeSize === size;
-                return (
-                  <li
-                    key={idx}
-                    className={active ? 'active' : ''}
-                    onClick={() => onChangeSize(size)}
-                  >
-                    {size} см.
-                    {active && size !== pizza.sizes[0] && (
-                      <div className='price-increase'>
-                        +{pizza.prices[pizza.types[0]][size] - pizza.startPrice} ₴
-                      </div>
-                    )}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div className='pizza-details__bottom'>
-            <div className='pizza-details__price'>от {currentPrice} ₴</div>
-            <button onClick={onClickAdd} className='button button--outline button--add'>
-              <svg
-                width='12'
-                height='12'
-                viewBox='0 0 12 12'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path
-                  d='M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z'
-                  fill='white'
-                />
-              </svg>
-              <span>Добавить</span>
-              {count > 0 && <i>{count}</i>}
-            </button>
+    <div className='pizza-details'>
+      <div className='container'>
+        <div className='pizza-details__wrapper'>
+          <img className='pizza-details__image' src={pizza.imageUrl} alt={pizza.title} />
+          <div className='pizza-details__info'>
+            <h2 className='pizza-details__title'>{pizza.title}</h2>
+            <div className='pizza-details__selector'>
+              <ul>
+                {pizza.types.map((typeId) => {
+                  const active = activeType === typeId;
+                  return (
+                    <li
+                      key={typeId}
+                      className={active ? 'active' : ''}
+                      onClick={() => onChangeType(typeId)}
+                    >
+                      {typeLabels[typeId]}
+                      {active && typeId !== pizza.types[0] && (
+                        <div className='price-increase'>
+                          +{pizza.prices[typeId][pizza.sizes[0]] - pizza.startPrice} ₴
+                        </div>
+                      )}
+                    </li>
+                  );
+                })}
+              </ul>
+              <ul>
+                {pizza.sizes.map((size, idx) => {
+                  const active = activeSize === size;
+                  return (
+                    <li
+                      key={idx}
+                      className={active ? 'active' : ''}
+                      onClick={() => onChangeSize(size)}
+                    >
+                      {size} см.
+                      {active && size !== pizza.sizes[0] && (
+                        <div className='price-increase'>
+                          +{pizza.prices[pizza.types[0]][size] - pizza.startPrice} ₴
+                        </div>
+                      )}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className='pizza-details__bottom'>
+              <div className='pizza-details__price'>от {currentPrice} ₴</div>
+              <button onClick={onClickAdd} className='button button--outline button--add'>
+                <svg
+                  width='12'
+                  height='12'
+                  viewBox='0 0 12 12'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    d='M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z'
+                    fill='white'
+                  />
+                </svg>
+                <span>Добавить</span>
+                {count > 0 && <i>{count}</i>}
+              </button>
+            </div>
           </div>
         </div>
+        <Link to='/' className='button button--black'>
+          <span>Вернуться назад</span>
+        </Link>
       </div>
     </div>
   );
